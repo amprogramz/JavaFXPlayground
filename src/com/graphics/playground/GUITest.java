799f95539634d831d8dd2c1fb04fa9658a64948e
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-03-13 12:27:15 -0600

    These are the basics to using JavaFX, you must understand that we use these methods from the abstract class application to begin.

---
+++ src/com/graphics/playground/GUITest.java
@@ -0,0 +1,35 @@
+package com.graphics.playground;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUITest extends Application {
+
+    public static void main(String[] args) {
+//      A static method which creates an instance of the app, then calls init(), start(), and stop() when code
+//      execution is finished.
+        launch(args);
+    }
+
+//    The start() method is derived from the abstract class Application. This is the entry point where the JavaFX code
+//    will be written.
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+    }
+
+//    The init() method is derived from the abstract class Application.
+    @Override
+    public void init()
+    {
+
+    }
+
+//    The stop() method is derived from the abstract class Application.
+    @Override
+    public void stop()
+    {
+
+    }
+}
